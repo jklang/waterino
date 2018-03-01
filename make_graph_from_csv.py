e71d7987fefbe4e7ac714935a9675e5df6c6f7f5
jklang <klang.joakim@gmail.com>
2018-03-01 22:04:16 +0100

    added new stuff

---
+++ make_graph_from_csv.py
@@ -0,0 +1,25 @@
+import json
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.figure_factory as FF
+import pandas as pd
+import plotly.graph_objs as go
+
+moisture_csv_file = '/Users/joakim/waterino/waterino/data/moisture.csv'
+water_level_csv_file = '/Users/joakim/waterino/waterino/data/water_level.csv'
+
+def update_graph(csv_file, title):
+    headers = ['DateTime','Sensor']
+    df = pd.read_csv(csv_file, names=headers)
+    with open('./conf/config.json') as config_file:
+        plotly_user_config = json.load(config_file)
+        py.sign_in(plotly_user_config["plotly_username"], plotly_user_config["plotly_api_key"])
+    trace = go.Scatter(x = df['DateTime'], y = df['Sensor'],
+        name='JoakimTest')
+    layout = go.Layout(title=title)
+    fig = go.Figure(data=[trace], layout=layout)
+    p = py.plot(fig, filename=title)
+    return p
+
+update_graph(moisture_csv_file, 'Soil Moisture Reading')
+update_graph(water_level_csv_file, "Water Level Reading")
---
+++ waterino.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import serial
+import time
+import plotly.plotly as py
+import plotly.graph_objs as go
+import requests
+import json
+import datetime
+
+
+def get_serial_output(ser):
+    v = ser.readline()
+    return v
+
+def send_notification(config, message):
+    # Make a request to the 46elks API
+    response = requests.post(
+        "https://api.46elks.com/a1/sms",
+        auth = (config["sms_gateway_username"], config["sms_gateway_password"]),
+        data = {
+            "from": "Waterino",
+            "to": config["sms_gateway_destination_phone_number"], 
+            "message": message
+        }
+    )
+    
+    # Check that the request was succesful
+    if response.status_code == 200:
+        return True
+    else:
+        # Log error?
+        return False
+
+def write_to_csv(filename, moisture):
+    with open(filename, 'a') as datafile:
+        datafile.write('{}, {}\n'.format(datetime.datetime.now(), moisture))
+        
+class Plotly:
+    def __init__(self, config):
+        py.sign_in(config['plotly_username'], config['plotly_api_key'])
+        py.plot([
+            {
+                'x': [], 'y': [], 'type': 'scatter',
+                'stream': {
+                    'token': config['plotly_streaming_tokens'][0],
+                    'maxpoints': 20000
+                }
+            }], filename='Current soil moisture value')
+    def write_to_streaming_graph(self, config, datetime, moisture):
+        stream = py.Stream(config['plotly_streaming_tokens'][0])
+        stream.open()
+        stream.write({'x': datetime, 'y': moisture})
+
+        
+def main():
+    moisture = 0
+    water_level = 0
+    ser = serial.Serial('/dev/tty.wchusbserial1410', 9600)
+    config_file = '/Users/joakim/waterino/waterino/conf/config.json'
+
+    with open(config_file) as conf:
+        config = json.load(conf)
+
+    p = Plotly(config)
+    while True:
+        moisture_stats_file = './data/moisture.csv'
+        water_level_stats_file = './data/water_level.csv'
+        value = get_serial_output(ser).strip('\n').strip('\r')
+        # Separate soil moisture (m) and water level(w) values into two seperate variables:
+        if 'm:' in value:
+            moisture = value.split(':')[1]
+        if 'w:' in value:
+            water_level = value.split(':')[1]
+    #   if water_level <= 20:
+    #        send_notification(config, 'Water level at {} Fill the water tank.'.format(water_level))
+        #if 'Watering' in value:
+        #    send_notification(config, value)
+	# Write graph data:
+        p.write_to_streaming_graph(config, datetime.datetime.now(), moisture)
+	write_to_csv(moisture_stats_file, moisture)
+	write_to_csv(water_level_stats_file, water_level)
+        print(moisture)
+
+if __name__ == "__main__":
+    main()
